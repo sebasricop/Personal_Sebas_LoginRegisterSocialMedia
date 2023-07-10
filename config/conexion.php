f6a94eeb6f09cea3504f702cf8eac6ade634d656
sebasricop <85714519+sebasricop@users.noreply.github.com>
2023-07-09 22:19:18 -0500

    Add conexion.php

---
+++ config/conexion.php
@@ -0,0 +1,25 @@
+<?
+
+class Conectar{
+     protected $dbh;
+
+     protected function conexion(){
+         try{
+            $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=loginsocialmedia","root","");
+            return $conectar;
+         }catch(Exception $e){
+             print "¡Error BD!: " . $e->getMessage() . "<br/>";
+             die();
+         }
+
+     }
+     public function set_names(){
+         return $this->dbh->query("SET NAMES 'utf8");
+     }
+
+     public function ruta(){
+         return "http://localhost:80/Personal_Sebas_LoginRegisterSocialMedia";
+     }
+}
+
+?>
